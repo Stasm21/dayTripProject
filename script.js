c25fcc4e8e3d969a9f082f7d8d49ee2ab7955740
Stas <stasm21@gmail.com>
2021-07-31 22:44:57 -0700

    functions for transportation and things to do finished

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let tripInfo = [];
 
 function destination() {
 
-    let places = ['Rome, Italy', 'Santorini, Greece', 'London, England', 'Paris, France', 'Lisbon, Portugal'];
+    let places = ['Mt. Hood for a breathtaking adventure', 'Portland Art Museum', 'Oregon Zoo', 'Pittock Mansion for an incredible view of the City', 'Trout fishing at Trillium lake'];
     let userInput = prompt(`For destination, pick a number 0 to ${places.length}`)
 
     for (let i = 0; i < places.length; i++) {
@@ -15,8 +15,10 @@ function destination() {
     // prompt(`For random destination, pick a number 0 to ${places.length}`)
 }
 
+
+
 function restaurant() {
-    let food = ['Steakhouse', 'Foodtruck', 'Vegan House', 'Old Country kitchen', 'Seafood'];
+    let food = ['Steakhouse', 'Foodtruck', 'Vegan House', 'Old Country kitchen', 'Seafood Restaurant'];
     let userInput = prompt(`For a place to eat, pick a number 0 to ${food.length}`)
 
     for (let i = 0; i < food.length; i++) {
@@ -25,22 +27,37 @@ function restaurant() {
             console.log(food[i])
         }
     }
-
 }
 
 
-function restaurant() {
-    let food = ['Steakhouse', 'Foodtruck', 'Vegan House', 'Old Country kitchen', 'Seafood'];
-    let userInput = prompt(`For a place to eat, pick a number 0 to ${food.length}`)
 
-    for (let i = 0; i < food.length; i++) {
+function transportation() {
+    let transport = ['Moped', 'taxi', 'Local bus', 'bicycle', 'car rental'];
+    let userInput = prompt(`For a means of transportation, pick a number 0 to ${transport.length}`)
+
+    for (let i = 0; i < transport.length; i++) {
         if (userInput == i) {
-            tripInfo.push(food[i])
-            console.log(food[i])
+            tripInfo.push(transport[i])
+            console.log(transport[i])
         }
     }
-
 }
+
+
+function entertainment() {
+    let things = ['on a historic tour of the City', 'wine and cheese tasting from the local vineyard', 'horseback riding', 'to a viewpoint of the city', 'to the Museum'];
+    let userInput = prompt(`For things to do, pick a number 0 to ${things.length}`)
+
+    for (let i = 0; i < things.length; i++) {
+        if (userInput == i) {
+            tripInfo.push(things[i])
+            console.log(things[i])
+        }
+    }
+}
+
+
+function
 
 
 // function doStuff(){
@@ -62,3 +79,6 @@ function restaurant() {
 
 destination();
 restaurant();
+transportation()
+entertainment()
+console.log(tripInfo)
