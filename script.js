9b7f4805a9743b22325dc822cc307062bb936d50
Stas <stasm21@gmail.com>
2021-08-02 07:06:26 -0700

    project finished

--- script.js
+++ script.js
@@ -1,84 +1,112 @@
 
-let tripInfo = [];
+
+let placesArr = [];
+let foodArr = [];
+let transArr = [];
+let entertainmentArr = [];
 
 function destination() {
 
-    let places = ['Mt. Hood for a breathtaking adventure', 'Portland Art Museum', 'Oregon Zoo', 'Pittock Mansion for an incredible view of the City', 'Trout fishing at Trillium lake'];
-    let userInput = prompt(`For destination, pick a number 0 to ${places.length}`)
+    let places = ['Rome, Italy', 'Santorini, Greece', 'London, England', 'Paris, France', 'Lisbon, Portugal'];
+    let userInput = prompt(`For destination, pick a number 0 to ${places.length - 1}`)
 
     for (let i = 0; i < places.length; i++) {
         if (userInput == i) {
-            tripInfo.push(places[i])
-            console.log(places[i])
+            placesArr.push(places[i])
+
         }
     }
-    // prompt(`For random destination, pick a number 0 to ${places.length}`)
+    if (userInput > places.length - 1) {
+        console.log('Please enter number 0 to 4')
+        destination();
+    }
 }
 
 
-
 function restaurant() {
-    let food = ['Steakhouse', 'Foodtruck', 'Vegan House', 'Old Country kitchen', 'Seafood Restaurant'];
-    let userInput = prompt(`For a place to eat, pick a number 0 to ${food.length}`)
+    let food = ['Steakhouse', 'Foodtruck', 'Noodle House', 'Old Country kitchen', 'Seafood Restaurant'];
+    let userInput = prompt(`For a place to eat, pick a number 0 to ${food.length - 1}`)
 
     for (let i = 0; i < food.length; i++) {
         if (userInput == i) {
-            tripInfo.push(food[i])
-            console.log(food[i])
+            foodArr.push(food[i])
+
         }
+    }
+    if (userInput > food.length - 1) {
+        console.log('Please enter number 0 to 4')
+        restaurant();
     }
 }
 
 
 
 function transportation() {
-    let transport = ['Moped', 'taxi', 'Local bus', 'bicycle', 'car rental'];
-    let userInput = prompt(`For a means of transportation, pick a number 0 to ${transport.length}`)
+    let transport = ['moped', 'taxi', 'local bus', 'Jeep', 'car rental'];
+    let userInput = prompt(`For means of transportation, pick a number 0 to ${transport.length - 1}`)
 
     for (let i = 0; i < transport.length; i++) {
         if (userInput == i) {
-            tripInfo.push(transport[i])
-            console.log(transport[i])
+            transArr.push(transport[i])
+
         }
+    }
+    if (userInput > transport.length - 1) {
+        console.log('Please enter number 0 to 4')
+        transportation();
     }
 }
 
 
 function entertainment() {
-    let things = ['on a historic tour of the City', 'wine and cheese tasting from the local vineyard', 'horseback riding', 'to a viewpoint of the city', 'to the Museum'];
-    let userInput = prompt(`For things to do, pick a number 0 to ${things.length}`)
+    let things = ['on an adventurous tour of the City', 'wine and cheese tasting from the local vineyard', 'horseback riding', 'to a panoramic viewpoint of the city', 'to the Historic Museum'];
+    let userInput = prompt(`For things to do, pick a number 0 to ${things.length - 1}`)
 
     for (let i = 0; i < things.length; i++) {
         if (userInput == i) {
-            tripInfo.push(things[i])
-            console.log(things[i])
+            entertainmentArr.push(things[i])
+
         }
+    }
+    if (userInput > things.length - 1) {
+        console.log('Please enter number 0 to 4')
+        entertainment();
     }
 }
 
+function itinerary() {
+    let userInput = prompt(`Tomorrow morning you you be flying out to ${placesArr[0]}. You will be taking a ${transArr[0]} to get around town. Your activity of the day will be going ${entertainmentArr[0]}, followed by dinner at the local ${foodArr[0]}. Please confirm if this sounds like a trip you would love to take? please type 'yes' or 'no'`);
 
-function
+    if (userInput === 'yes') {
+        console.log('Your Trip has been Booked!!!!!!!, please check email for details!')
+    } else if (userInput === 'no') {
+        let changeOption = prompt("Which part of the trip didn't you like? For destination Press 1, Restaurant Press 2, Transportation Press 3, Entertainment Press 4");
+        if (changeOption == 1) {
+            destination();
+            placesArr.shift();
+            itinerary();
+        } else if (changeOption == 2) {
+            restaurant();
+            foodArr.shift();
+            itinerary();
+        } else if (changeOption == 3) {
+            transportation();
+            transArr.shift();
+            itinerary();
+        } else if (changeOption == 4) {
+            entertainment();
+            entertainmentArr.shift();
+            itinerary();
+        }
+    } else {
+        itinerary();
+    }
 
+}
 
-// function doStuff(){
-//     let colors = ["red", "blue", "green"]
-//     let userInput = prompt("What color are you looking for?")
-//      let isFound = false;
-//     for (let i = 0; i < colors.length; i++) {
-//         if(colors[i] == userInput){
-//             console.log(`We found the color ${userInput}`)
-//              isFound = true;
-//         }
-//     }
-//     if(isFound == false){
-//         console.log("We dont have that color in the array")
-//     }
-//  }
-
-// doStuff();
 
 destination();
 restaurant();
 transportation()
 entertainment()
-console.log(tripInfo)
+itinerary()
